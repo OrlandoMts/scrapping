e74df5e76088a20b87ff2574cea2689b2a744e42
Orlando Montes <omontes@indelpro.com>
2024-10-17 09:14:10 -0600

    2.El scrapper avanza en la paginación

--- 04-scrapper.py
+++ 04-scrapper.py
@@ -1,28 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
+# URL de la página web
+sitio = "https://stackoverflow.com"
+url = "/questions"
+page = 0
 
-url = "https://stackoverflow.com/questions"
-res = requests.get(url, verify=False)
-text = res.text
+# while url:
+while page < 3:
+    res = requests.get(sitio + url, verify=False)
+    text = res.text
 
-soup = BeautifulSoup(text, "html.parser")
-questions = soup.select(".s-post-summary")
-pagination = soup.select(".s-pagination")
-# print(questions[0]["data-post-id"])
-# print(pagination[0])
+    # Parsear el contenido HTML
+    soup = BeautifulSoup(text, 'html.parser')
+    # Seleccionar todas las preguntas
+    questions = soup.select('.s-post-summary')
 
-for pag in pagination:
-    print("************")
-    print(pag)
-    # print(pag.get_text())
-    # page = pag.select_one(".s-pagination--item").get_text()
-    # print(page)
+    for question in questions:
+        title = question.select_one(".s-link").get_text()
+        user = question.select_one(".s-user-card--link").get_text()
+        # vote_count = question.select_one(".s-post-summary--stats-item-number").get_text()
+        print(f"{user} - Titulo: \n{title.strip()}")
 
-for question in questions:
-    title = question.select_one(".s-link").get_text()
-    user = question.select_one(".s-user-card--link").get_text()
-    # vote_count = question.select_one(
-    #     ".s-post-summary--stats-item-number").get_text()
-    # print(title, vote_count)
-    # print(f"{user} - Titulo: \n{title.strip()}")
+    next_page = None
+    pagination = soup.select(".s-pagination--item")
+
+    for pag in pagination:
+        if pag.get_text().strip() == "Next":
+            next_page = pag.get("href")
+            break
+
+    if next_page:
+        url = next_page
+        print(f"Pasando a la siguiente página: {url}")
+    else:
+        print("No hay más páginas disponibles.")
+        break
+    print("=============================================")
+    page += 1
